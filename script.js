6b3c943a4c1bcc77fdb2da5123b4e7874a847264
Stephen Mayeux <stephenmayeux@gmail.com>
2018-02-27 07:51:54 -0600

    send and recieve messages

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Initial Variables
 var myUsername = null;
 var initMessages = null;
 var socket = io('http://localhost:3000');
@@ -15,7 +16,7 @@ function enterChatroom() {
   });
 
   // Scroll to bottom of page to see most recent messages
-  window.scrollTo(0,document.body.scrollHeight);
+  window.scrollTo(0, document.body.scrollHeight);
 }
 
 // determine if username is valid or not
@@ -66,6 +67,7 @@ $(document).ready(function() {
     }
   });
 
+  // Pressing Enter key allows us to enter the room
   $('#usernameModalInput').on('keyup', function(e) {
     if (e.keyCode === 13 && myUsername) {
       enterChatroom();
@@ -75,8 +77,29 @@ $(document).ready(function() {
   // Close modal and enter chat room
   $('#enterRoomBtn').on('click', enterChatroom);
 
+  // Send messages
+  $('#sendMessageBtn').on('click', function() {
+    var payload = {
+      u: myUsername,
+      t: Date.now(),
+      m: $('#chatMessageInput').val()
+    }
+
+    buildMyChatBubble(payload);
+    window.scrollTo(0, document.body.scrollHeight);
+    $('#chatMessageInput').val('')
+
+    socket.emit('sendMessage', payload);
+  });
+
   // Load recent messages from server to chat room
   socket.on('initMessages', function(messages) {
     initMessages = messages.map(message => JSON.parse(message));
+  });
+
+  // Receive messages sent by others
+  socket.on('newMessage', function(message) {
+    buildTheirChatBubble(JSON.parse(message));
+    window.scrollTo(0, document.body.scrollHeight);
   });
 });
