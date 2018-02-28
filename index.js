41d43cd316fdf597221d068d0de31d093357fdf9
Stephen Mayeux <stephenmayeux@gmail.com>
2018-02-27 18:28:41 -0600

    merge final project

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const socket = require('socket.io');
+const redisClient = require('./lib/redis');
 
 const app = express();
 const server = http.createServer(app);
@@ -13,6 +14,25 @@ app.get('*', (req, res) => {
 });
 
 const io = socket.listen(server);
+
+redisClient().then(connector => {
+
+  io.on('connection', socket => {
+    // Send 10 most recent messages to connecting client
+    connector.lrangeAsync('messages', -10, -1).then(messages => {
+      socket.emit('initMessages', messages)
+    });
+
+    // the sending client has already added their message directly to the DOM.
+    // Broadcast this event to everyone except for the sending client.
+    socket.on('sendMessage', (message) => {
+      connector.rpushAsync('messages', JSON.stringify(message)).then(response => {
+        socket.broadcast.emit('newMessage', JSON.stringify(message));
+      });
+    })
+  });
+
+});
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
